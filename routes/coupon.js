94b1e6dfe0d432010defe0e19426747731b8394d
JalinWu <lily.wu035@gmail.com>
2023-02-24 00:31:19 +0800

    complete ssrf functions

--- routes/coupon.js
+++ routes/coupon.js
@@ -7,39 +7,27 @@ const { forwardAuthenticated, ensureAuthenticated } = require('../config/auth');
 // var point = 100;
 // var exchangeList = [];
 
-// http://localhost:5000/coupon/..%2FDucyrqW8Sd
-// router.get('/:coupon', (req, res) => {
-//     console.log(req.params.coupon);
-
-//     var coupon = "coupon-test";
-//     res.send(coupon);
-// });
-
 router.get('/', ensureAuthenticated, (req, res) => {
-    // todo: get coupon from internal
-    axios.get('http://localhost:5000/internal/coupon/get')
+    // get coupon from internal
+    axios.get('http://localhost:5000/internal/coupon')
         .then(response => {
-            console.log(response);
-            // console.log(response.data.url);
-            // console.log(response.data.explanation);
+            res.send(response.data);
         })
         .catch(error => {
-            console.log(error);
+            res.send(error);
         });
-
-    var coupon = "coupon-test";
-    res.send(coupon);
 });
 
 router.post('/:coupon', ensureAuthenticated, (req, res) => {
     var { coupon } = req.params;
-    console.log(coupon);
-    // todo: post coupon to internal
-    // console.log(`http://localhost:5000/internal/coupon/../point`);
-    // console.log(`http://localhost:5000/internal/coupon/${coupon}`);
-    // todo: post coupon to internal
-    var coupon = "coupon-test";
-    res.send(coupon);
+    // post coupon to internal
+    axios.post(`http://localhost:5000/internal/coupon/${coupon}`)
+        .then(response => {
+            res.send(response.data);
+        })
+        .catch(error => {
+            res.send(error);
+        });
 });
 
 
